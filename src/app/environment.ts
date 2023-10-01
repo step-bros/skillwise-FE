ff9851af9789fc94399ecedf351234f9e68bce33
Lee Jia Keat <leejiakeat@gmail.com>
2023-10-01 20:42:17 +0800

    updatae api

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -1,4 +1,4 @@
 export const DEV_API_PATH = '/api/';
-export const PROD_API_PATH = 'https://tanzu-java-web-app-apps.tapb-adjusted-panda.tapsandbox.com/';
+export const PROD_API_PATH = 'https://tanzu-java-web-app-apps.tapb-grateful-dassie.tapsandbox.com/';
 export const IS_DEV = false;
 export const API_PATH = IS_DEV ? DEV_API_PATH : PROD_API_PATH;
